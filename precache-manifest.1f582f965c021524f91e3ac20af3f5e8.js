a65aeafa2bc458b50bb645311e07e11e81301399
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-04-05 16:14:26 -0700

    Updates

---
+++ precache-manifest.1f582f965c021524f91e3ac20af3f5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boo-animation/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d6485433e0e7bb5908d67b5cf3a73a0",
+    "url": "/boo-animation/static/media/boo.8d648543.jpg"
+  },
+  {
+    "revision": "ed9c0c39ad7573e9d937",
+    "url": "/boo-animation/static/js/runtime~main.09319e3b.js"
+  },
+  {
+    "revision": "6c5ec37357677c04ae15",
+    "url": "/boo-animation/static/js/main.36388312.chunk.js"
+  },
+  {
+    "revision": "a4bfaf8c4bccdd0a2cf9",
+    "url": "/boo-animation/static/js/2.0771dcc1.chunk.js"
+  },
+  {
+    "revision": "6c5ec37357677c04ae15",
+    "url": "/boo-animation/static/css/main.f758d62a.chunk.css"
+  },
+  {
+    "revision": "4628974912ad3aad119841cff75133d2",
+    "url": "/boo-animation/index.html"
+  }
+];
